0027299f6aeff1e900d804904036c56d460a24b5
Baose162002 <btran438@gmail.com>
2025-03-31 15:39:05 +0700

    update server 9

--- twilio.js
+++ twilio.js
@@ -1,7 +1,7 @@
 const twilio = require("twilio");
 
 // Sử dụng thông tin tài khoản Twilio của bạn
-const accountSid = "ACC4055aedb5e35b3daff470b3d2708495";
+const accountSid = "ACc4055aedb5e35b3da1f470b3d2708495";
 const authToken = "41e285dd875332daa2ebbac98548d63c";
 
 // Tạo API endpoint để lấy TURN credentials
