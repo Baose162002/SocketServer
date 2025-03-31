1b1c82885db75cc3862ba2024da373df6ca5d830
Baose162002 <btran438@gmail.com>
2025-03-31 16:41:57 +0700

    update server 13

--- twilio.js
+++ twilio.js
@@ -5,34 +5,45 @@ const getTwilioTurnCredentials = () => {
       { urls: "stun:stun.l.google.com:19302" },
       { urls: "stun:stun1.l.google.com:19302" },
       { urls: "stun:stun2.l.google.com:19302" },
+      { urls: "stun:stun3.l.google.com:19302" },
+      { urls: "stun:stun4.l.google.com:19302" },
       { urls: "stun:global.stun.twilio.com:3478" },
+
+      // Thêm các TURN server mới
+      {
+        urls: "turn:turn.anyfirewall.com:443?transport=tcp",
+        username: "webrtc",
+        credential: "webrtc",
+      },
+      {
+        urls: "turn:turn.anyfirewall.com:443",
+        username: "webrtc",
+        credential: "webrtc",
+      },
+      {
+        urls: "turn:numb.viagenie.ca",
+        username: "webrtc@live.com",
+        credential: "muazkh",
+      },
+      {
+        urls: "turn:192.158.29.39:3478?transport=udp",
+        username: "28224511:1379330808",
+        credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+      },
+      {
+        urls: "turn:192.158.29.39:3478?transport=tcp",
+        username: "28224511:1379330808",
+        credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+      },
+
+      // Giữ lại một số TURN server cũ
       {
         urls: "turn:openrelay.metered.ca:80",
         username: "openrelayproject",
         credential: "openrelayproject",
       },
       {
-        urls: "turn:openrelay.metered.ca:443",
-        username: "openrelayproject",
-        credential: "openrelayproject",
-      },
-      {
-        urls: "turn:openrelay.metered.ca:443?transport=tcp",
-        username: "openrelayproject",
-        credential: "openrelayproject",
-      },
-      {
         urls: "turn:relay.metered.ca:80",
-        username: "83eebabf8bf7d2c252ee6c3d",
-        credential: "OZoV0r7Ien5ZdLkA",
-      },
-      {
-        urls: "turn:relay.metered.ca:443",
-        username: "83eebabf8bf7d2c252ee6c3d",
-        credential: "OZoV0r7Ien5ZdLkA",
-      },
-      {
-        urls: "turn:relay.metered.ca:443?transport=tcp",
         username: "83eebabf8bf7d2c252ee6c3d",
         credential: "OZoV0r7Ien5ZdLkA",
       },
